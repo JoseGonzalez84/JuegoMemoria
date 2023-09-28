bd090c52509a687c123fab8c576734cd45f03bf5
Jose Gonzalez <josegs84@gmail.com>
2023-09-29 01:09:47 +0200

    First upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once "src/Tablero.class.php";
+require_once "src/Carta.class.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <title>Juego de Memoria</title>
+</head>
+<body>
+    <?php new Tablero(true); ?>
+</body>
+</html>
---
+++ src/Tablero.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * 
+ */
+class Tablero
+{
+    private array $_contenido;
+    public const NUMERO_CARTAS = 24;
+
+    private function outputFormateado(mixed $dato) {
+        print_r('<pre>');
+        var_dump($dato);
+        print_r('</pre>');
+    }
+
+    public function __construct(bool $draw=false)
+    {
+        if ($draw === true) {
+            echo $this->draw();
+        }
+    }
+
+    public function draw()
+    {
+        // Se van a obtener los identificadores de las cartas para jugar.
+        $cartasJuego = [];
+        for ($i=0; $i < (self::NUMERO_CARTAS / 2); $i++) {
+            $personajeAleatorio = Carta::eligePersonajeAzar();
+            if (in_array($personajeAleatorio, $cartasJuego) === false) {
+                $cartasJuego[] = $personajeAleatorio;
+                $cartasJuego[] = $personajeAleatorio;
+            } else {
+                $i--;
+            }
+        }
+        // Se mezclan.
+        shuffle($cartasJuego);
+        // Se dibuja el tablero.
+        $cartasCompletas = '<div style="display: flex;width: 75%;margin: 0 auto;">';
+        for ($i=0; $i < 6; $i++) {
+            $cartasCompletas .= '<div style="flex: 1 1 auto;">';
+            for ($l=0; $l < 4; $l++) {
+                $cartaElegida = array_pop($cartasJuego);
+                $carta = new Carta($cartaElegida);
+                $cartasCompletas .= $carta->draw();
+            }
+            $cartasCompletas .= '</div>';
+        }
+        $cartasCompletas .= '</div>';
+
+        return $cartasCompletas;
+    }
+}
